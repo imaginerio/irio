1b4c6c205309b7d9b6d491df8c2b91d54ab978bb
David Heyman <dave@axismaps.com>
2018-08-20 17:07:15 +0100

    Run bash as sudo to install Mapnik

--- install.sh
+++ install.sh
@@ -21,14 +21,14 @@ export CXX="clang++-3.8" && export CC="clang-3.8"
 
 git clone https://github.com/mapnik/mapnik mapnik-3.x --depth 10
 
+sudo bash
 cd mapnik-3.x
 git submodule update --init
 source bootstrap.sh
 ./configure CUSTOM_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" CXX=${CXX} CC=${CC}
 make
 make install
-
-cd ../
+exit
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs build-essential libcairo2-dev libjpeg-dev libgif-dev
